6323de8cb7180729a43b89aa2b3304131595e4ad
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2018-07-17 23:56:02 -0500

    MEJORA EN SEGURIDAD

--- php/dashboard/storage_new_sql.php
+++ php/dashboard/storage_new_sql.php
@@ -14,17 +14,35 @@
   require_once("config/parameters.php");
   require_once("config/connection.php");
 
-  if (is_uploaded_file($_FILES["file"]["tmp_name"])){
-    move_uploaded_file($_FILES["file"]["tmp_name"], "storage/" . $_FILES["file"]["name"]);
+  if(isset($_FILES["file"]["name"])) {
 
-    $query = $mysql->prepare("INSERT INTO storage (id, description, file, priority) VALUES (NULL, :description, :file, :priority)");
-    $query->execute([
-      ':description' => $_POST['description'],
-      ':file' => $_FILES["file"]["name"],
-      ':priority' => $_POST['priority']
-    ]);
+    $extension = strtolower(end(explode(".", $_FILES["file"]["name"])));
+    
+    if($extension != "php"){
+      
+       if (is_uploaded_file($_FILES["file"]["tmp_name"])){
+        move_uploaded_file($_FILES["file"]["tmp_name"], "storage/" . $_FILES["file"]["name"]);
 
-    if($query) header('location: storage.php');
-  } else {
-    header('location: storage.php');
+        $query = $mysql->prepare("INSERT INTO storage (id, description, file, priority) VALUES (NULL, :description, :file, :priority)");
+        $query->execute([
+          ':description' => $_POST['description'],
+          ':file' => $_FILES["file"]["name"],
+          ':priority' => $_POST['priority']
+        ]);
+
+        if($query) header('location: storage.php');
+      } else {
+        header('location: storage.php');
+      }
+
+    }else{
+      echo "ERROR";
+      exit;
+    }
+
+
+
+  }else{
+    echo "ERROR";
+    exit;
   }
