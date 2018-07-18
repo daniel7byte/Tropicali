8fc3921f22478acaf1a8be8065a167c6f4406e18
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2018-07-18 11:03:24 -0500

    ADIOS IMAGEN

--- php/dashboard/navbar.php
+++ php/dashboard/navbar.php
@@ -23,7 +23,6 @@
   </div>
   <div class="dropdown navbar-dropdown">
     <a class="dropdown-toggle navbar-dropdown-toggle navbar-dropdown-toggle__user" data-toggle="dropdown" href="#">
-      <img src="img/users/user-3.png" alt="" class="navbar-dropdown-toggle__user-avatar">
       <span class="navbar-dropdown__user-name"><?=$_SESSION['full_name']?></span>
     </a>
     <div class="dropdown-menu navbar-dropdown-menu navbar-dropdown-menu__user">
